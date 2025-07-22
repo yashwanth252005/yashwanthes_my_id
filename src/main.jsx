8b0abdab2b82e1698d6370f4be03580433efc440
Yashwanth E S <yashwanthes25@gmail.com>
2025-07-22 12:49:08 +0530

    set_vercel_speed

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
@@ -6,5 +7,6 @@ import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
+		<SpeedInsights />
 	</React.StrictMode>,
 )
